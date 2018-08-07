7be65019c749583a72bef16fd9a9760367068315
am903103 <am903103@au.edu.tw>
2018-08-07 11:40:58 +0800

    Refactory test unit function to extension

---
+++ SimpleNoteTakingAppTests/Steps/LoginSteps.swift
@@ -0,0 +1,24 @@
+//
+//  LoginSteps.swift
+//  SimpleNoteTakingAppTests
+//
+//  Created by Apple on 2018/8/7.
+//  Copyright © 2018年 gamma404. All rights reserved.
+//
+
+import Foundation
+
+extension LoginTests {
+    func clearOutUsernameAndPasswordFields() {
+        tester().clearTextFromView(withAccessibilityLabel: "Login - Username")
+        tester().clearTextFromView(withAccessibilityLabel: "Login - Password")
+    }
+
+    func tapButton(buttonName: String) {
+        tester().tapView(withAccessibilityLabel: buttonName)
+    }
+
+    func expectToSeeAlert(text: String) {
+        tester().waitForView(withAccessibilityLabel: (text))
+    }
+}
